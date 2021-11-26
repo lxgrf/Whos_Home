f9ce46da95f00ff0cb8703dbd544b00c7f5cc066
lxgrf <52136105+lxgrf@users.noreply.github.com>
2021-11-26 16:12:23 +0000

    Update WiFiPing.py
    Alterations to the time checking functions, making better use of the time module.

--- WiFiPing.py
+++ WiFiPing.py
@@ -1,53 +1,63 @@
 import subprocess
-import time as sleep
-import RPi.GPIO as GPIO # Gives access to the digital inputs/outputs on the Pi
-from datetime import datetime
- 
+import time
+import RPi.GPIO as GPIO  # Gives access to the digital inputs/outputs on the Pi
+
+
 #### KEY VARIABLES ###
-#Operation times (Hours between which the lights are enabled)
-start = time(6)
-end = time(22)
- 
-#IP Addresses - you'll want to set your devices to a static IP on your router.
+# Operation times (Hours between which the lights are enabled)
+start = 6
+end = 22
+
+# IP Addresses - you'll want to set your devices to a static IP on your router.
 ipA = "192.168.1.100"
 ipL = "192.168.1.101"
- 
-#How many failed pings before a device is shown as disconnected. Recommend >1, pings aren't always reliable!
+
+# How many failed pings before a device is shown as disconnected. Recommend >1, pings aren't always reliable!
 cooldown = 15
 
-#How long between pings, in seconds
+# How long between pings, in seconds
 interval = 60
 
 
- 
 #### FUNCTIONS ###
 def ping(IP):
-              result = subprocess.call(["ping", IP, "-c1", "-W2", "-q"])
-              if result == 0: return True
-              else: return False
- 
-def led(pin,state):
-              if state: GPIO.output(pin, GPIO.HIGH)
-              else: GPIO.output(pin, GPIO.LOW)
- 
+    result = subprocess.call(["ping", IP, "-c1", "-W2", "-q"])
+    if result == 0:
+        return True
+    else:
+        return False
+
+
+def led(pin, state):
+    if state:
+        GPIO.output(pin, GPIO.HIGH)
+    else:
+        GPIO.output(pin, GPIO.LOW)
+
+
 def reset():
-        led(2,False)
-        led(3,False)
-        led(4,False)
-        countA, countL = 0, 0
- 
-def in_between(now): # Returns True if in hours of operation
-              if start <= end: return start <= now < end
-              else: return start <= now or now < end
- 
-def wait(duration): sleep.sleep(duration)
- 
+    led(2, False)
+    led(3, False)
+    led(4, False)
+    return 0, 0
+
+
+def in_between(now):  # Returns True if in hours of operation
+    if start <= end:
+        return start <= now.tm_hour < end
+    else:
+        return start <= now.tm_hour or now.tm_hour < end
+
+
+def wait(duration): time.sleep(duration)
+
+
 def flash():
-              for x in range(0,6):
-                            led(3,True)
-                            wait(1)
-                            led(3,False)
-                            wait(1)
+    for _ in range(0, 6):
+        led(3, True)
+        wait(1)
+        led(3, False)
+        wait(1)
 
 
 ### SETUP ###
@@ -56,32 +66,37 @@ GPIO.setwarnings(False)
 GPIO.setup(2, GPIO.OUT)
 GPIO.setup(3, GPIO.OUT)
 GPIO.setup(4, GPIO.OUT)
-reset()
-for x in range(0,6): #Flash the Ampersand to show it's booting
-              led(3,True)
-              wait(1)
-              led(3,False)
-              wait(1)
+countA, countL = reset()
 
+for x in range(0, 6):  # Flash the Ampersand to show it's booting
+    led(3, True)
+    wait(1)
+    led(3, False)
+    wait(1)
 
-### MAIN LOOP ###      
+### MAIN LOOP ###
 while True:
-              if in_between(datetime.now().time()): #Check current time is between the functional hours
-                            if ping(ipA): countA = cooldown # Ping successful, reset count
-                            elif countA > 0: countA -= 1 #Ping unsuccessful, decrement count
- 
-                            if ping(ipL): countL = cooldown
-                            elif countL > 0: countL -= 1
- 
-                            #Light 'A' if counter not expired
-                            led(4,countA > 0)
- 
-                            #Light 'L' if counter not expired
-                            led(2,countL > 0)
- 
-                            #Light '&' if both
-                            led(3, countA > 0 and countL > 0)
- 
-              else: reset()
- 
-              wait(interval)
+    if in_between(time.localtime()):  # Check current time is between the functional hours
+        if ping(ipA):
+            countA = cooldown  # Ping successful, reset count
+        elif countA > 0:
+            countA -= 1  # Ping unsuccessful, decrement count
+
+        if ping(ipL):
+            countL = cooldown
+        elif countL > 0:
+            countL -= 1
+
+        # Light 'A' if counter not expired
+        led(4, countA > 0)
+
+        # Light 'L' if counter not expired
+        led(2, countL > 0)
+
+        # Light '&' if both
+        led(3, countA > 0 and countL > 0)
+
+    else:
+        countA, countL = reset()
+
+    wait(interval)
